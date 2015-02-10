c132b6526db04964ea377900c2852c45e34b395a
antoineodier <odier@hab.de>
2015-02-10 15:18:01 +0100

    fin-1-avant_refacto

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,12 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
   def index
     @tasks = Task.all
   end
@@ -15,4 +21,23 @@ class TasksController < ApplicationController
   def edit
     @task = Task.find(params[:id])
   end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :content, :time, :date, :place)
+  end
+
 end
