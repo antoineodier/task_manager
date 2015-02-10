d6b1cbd3603288d3062ab9262eb4a3e6f7b1bfbe
antoineodier <odier@hab.de>
2015-02-10 14:35:06 +0100

    milieu-1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,18 @@
 class TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+  end
+
   def index
     @tasks = Task.all
   end
 
   def show
-  end
-
-  def new
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 end
