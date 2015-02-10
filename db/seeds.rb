ef4579a4856b7b4c8eec91eece83df07ec46e24f
antoineodier <odier@hab.de>
2015-02-10 12:09:45 +0100

    nvel-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,9 @@
-
-
 20.times do
 
 task = Task.new(
     name: Faker::Commerce.product_name,
     place: Faker::Address.street_address,
-    content: Faker::Lorem.sentences(4),
+    content: Faker::Lorem.paragraph,
     date: Faker::Date.forward(300),
     time: "#{Faker::Number.digit}h#{Faker::Number.digit}#{Faker::Number.digit}"
     )
